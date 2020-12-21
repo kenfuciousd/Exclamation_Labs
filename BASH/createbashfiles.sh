555330d6d6a3346b78372a5bca9603ca3aa48665
kenfuciousd <35818192+kenfuciousd@users.noreply.github.com>
2020-12-21 10:28:58 -0600

    completed bash with outputs
    updating bash outputs

--- BASH/createbashfiles.sh
+++ BASH/createbashfiles.sh
@@ -12,6 +12,7 @@ do
 	echo "we are adding" $FN
 	touch $FN
 	#Append the text entries into the files
+	# one issue - this will continue appending FileEntries into the files each time.
 	for FE in ${FILEENTRIES[*]} 
 	do
 		echo $FE >> $FN
@@ -24,11 +25,11 @@ done
 LS -lisa
 
 #object cleanup during testing
-for FN in ${FILENAMES[*]}
-do
-	echo "we are removing" $FN
-	rm $FN
-done
-	
-#check to see what was done.
-LS -lisa
+#for FN in ${FILENAMES[*]}
+#do
+#	echo "we are removing" $FN
+#	rm $FN
+#done
+
+#check to see if the files were removed
+#LS -lisa
